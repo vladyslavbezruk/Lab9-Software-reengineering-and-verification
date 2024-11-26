bdc9bb51ace8cb58c17f04ba2717575abde25271
Vladyslav Bezruk <vladyslavbezruk@yahoo.com>
2024-11-26 23:13:23 +0200

    Task 3

--- src/main/java/edu/sumdu/monopoly/GameMaster.java
+++ src/main/java/edu/sumdu/monopoly/GameMaster.java
@@ -52,13 +52,7 @@ public class GameMaster {
 		setAllButtonEnabled(false);
 		getCurrentPlayer().getPosition().playAction();
 		if(getCurrentPlayer().isBankrupt()) {
-			gui.setBuyHouseEnabled(false);
-			gui.setDrawCardEnabled(false);
-			gui.setEndTurnEnabled(false);
-			gui.setGetOutOfJailEnabled(false);
-			gui.setPurchasePropertyEnabled(false);
-			gui.setRollDiceEnabled(false);
-			gui.setTradeEnabled(getCurrentPlayerIndex(),false);
+			setAllButtonDisabled();
 			updateGUI();
 		}
 		else {
@@ -70,13 +64,7 @@ public class GameMaster {
     public void btnGetOutOfJailClicked() {
 		getCurrentPlayer().getOutOfJail();
 		if(getCurrentPlayer().isBankrupt()) {
-			gui.setBuyHouseEnabled(false);
-			gui.setDrawCardEnabled(false);
-			gui.setEndTurnEnabled(false);
-			gui.setGetOutOfJailEnabled(false);
-			gui.setPurchasePropertyEnabled(false);
-			gui.setRollDiceEnabled(false);
-			gui.setTradeEnabled(getCurrentPlayerIndex(),false);
+			setAllButtonDisabled();
 		}
 		else {
 			gui.setRollDiceEnabled(true);
@@ -84,6 +72,16 @@ public class GameMaster {
 			gui.setGetOutOfJailEnabled(getCurrentPlayer().isInJail());
 		}
     }
+
+	private void setAllButtonDisabled() {
+		gui.setBuyHouseEnabled(false);
+		gui.setDrawCardEnabled(false);
+		gui.setEndTurnEnabled(false);
+		gui.setGetOutOfJailEnabled(false);
+		gui.setPurchasePropertyEnabled(false);
+		gui.setRollDiceEnabled(false);
+		gui.setTradeEnabled(getCurrentPlayerIndex(),false);
+	}
 
     public void btnPurchasePropertyClicked() {
         Player player = getCurrentPlayer();
