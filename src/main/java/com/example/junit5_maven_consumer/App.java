bef12de5d47595a392f46954ea5c58f2c249348b
Vladyslav Bezruk <vladyslavbezruk@yahoo.com>
2024-11-26 23:01:58 +0200

    Initial commit.

---
+++ src/main/java/com/example/junit5_maven_consumer/App.java
@@ -0,0 +1,10 @@
+package com.example.junit5_maven_consumer;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/example/junit5_maven_consumer/AppTest.java
@@ -0,0 +1,19 @@
+package com.example.junit5_maven_consumer;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
