f1bcd82b148110c211513dccff028ea722f658f6
Vladyslav Bezruk <vladyslavbezruk@yahoo.com>
2024-11-26 23:16:45 +0200

    Task 4

--- src/main/java/edu/sumdu/monopoly/PropertyCell.java
+++ src/main/java/edu/sumdu/monopoly/PropertyCell.java
@@ -25,14 +25,19 @@ public class PropertyCell extends Cell {
 
 	public int getRent() {
 		int rentToCharge = rent;
+		rentToCharge = rentForMonopolies(rentToCharge);
+		if(numHouses > 0) {
+			rentToCharge = rent * (numHouses + 1);
+		}
+		return rentToCharge;
+	}
+
+	private int rentForMonopolies(int rentToCharge) {
 		String [] monopolies = owner.getMonopolies();
 		for(int i = 0; i < monopolies.length; i++) {
 			if(monopolies[i].equals(colorGroup)) {
 				rentToCharge = rent * 2;
 			}
-		}
-		if(numHouses > 0) {
-			rentToCharge = rent * (numHouses + 1);
 		}
 		return rentToCharge;
 	}
